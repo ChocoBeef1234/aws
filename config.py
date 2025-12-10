77c5feb7d6037074a5c759b6a3aa5e49fb86bc50
ChocoBeef1234 <144603921+ChocoBeef1234@users.noreply.github.com>
2025-12-10 15:14:02 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "database-1.cbk9pytreoek.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "aws_user"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "liewkimwah-employee"
 customregion = "us-east-1"
 
